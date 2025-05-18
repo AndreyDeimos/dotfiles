c9016357f236cf2f37aa4aae52a5580b363c87f2
Andrey <and.mi.eremenko@gmail.com>
2025-05-18 20:12:42 +0300

    added separators to status bar

--- .config/ignis/modules/status_window.py
+++ .config/ignis/modules/status_window.py
@@ -6,6 +6,8 @@ fetch_service = FetchService.get_default()
 
 
 def Stats(monitor):
+    separator1 = Widget.Label(label="|")
+    separator2 = Widget.Label(label="|")
     logo = Widget.Icon(image=fetch_service.bind("os_logo"))
     cpu = Widget.Label(label=fetch_service.bind("cpu_temp", lambda x: f"  : {x}"))
     mem = Widget.Box(
@@ -19,9 +21,11 @@ def Stats(monitor):
                 css_classes=["slider"],
             ),
         ],
-        spacing=12,
+        spacing=15,
     )
-    box = Widget.Box(child=[logo, cpu, mem], css_classes=["bar"], spacing=13)
+    box = Widget.Box(
+        child=[logo, separator1, cpu, separator2, mem], css_classes=["bar"], spacing=13
+    )
     modules.windows.BackgroundWindow(
         "stats", monitor, box, {"anchor": ["bottom"], "margin_bottom": 20}
     )
